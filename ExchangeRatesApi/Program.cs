cfd810bf39646bcc08f445b31c78b873c87d5882
hdoupe <hdoupe@taxslayer.com>
2025-09-18 14:36:05 -0700

    Fix swagger documentation

--- ExchangeRatesApi/Program.cs
+++ ExchangeRatesApi/Program.cs
@@ -26,7 +26,10 @@ builder.Services.AddDbContext<ExchangeRatesContext>(opt => {
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.CustomSchemaIds(type => type.FullName);
+});
 
 var app = builder.Build();
 app.UseCors("ApiCorsPolicy");
